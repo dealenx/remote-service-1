cb0e151623af9416c4970b5fcca5c56a911e2d7e
Daniil dealenx Gorodilov <dealenx@gmail.com>
2022-10-28 01:16:52 +0700

    fix: added public folder and index.html file

--- routes/routes.ts
+++ routes/routes.ts
@@ -1,13 +1,38 @@
 import { Router } from "https://deno.land/x/oak/mod.ts";
-import { getTodoItem, getTodos, saveTodo, updateTodoItem, deleteTodoItem } from "../controllers/todos.ts";
+import * as path from "https://deno.land/std@0.146.0/path/mod.ts";
+import {
+  getTodoItem,
+  getTodos,
+  saveTodo,
+  updateTodoItem,
+  deleteTodoItem,
+} from "../controllers/todos.ts";
+
+// A helper function to get the file contents
+// of a specific file path in the public directory
+function getPublicFile(...filePath: string[]): Promise<Uint8Array> {
+  return Deno.readFile(path.join("./public", ...filePath));
+}
 
 const router = new Router();
 
+router.get("/", async (ctx, next) => {
+  // Set the contents of the "index.html" file to the response body
+  ctx.response.body = await getPublicFile("index.html");
+
+  // Set the appropriate resopnse type for HTML
+  ctx.response.type = "text/html";
+
+  // This isn't technically needed here, but it's good practice
+  // because other middleware might need to run in more complicated routes
+  await next();
+});
+
 router
-    .get("/todo", getTodos)
-    .get("/todo/:id", getTodoItem)
-    .post("/todo", saveTodo)
-    .put("/todo/:id", updateTodoItem)
-    .delete("/todo/:id", deleteTodoItem);
+  .get("/todo", getTodos)
+  .get("/todo/:id", getTodoItem)
+  .post("/todo", saveTodo)
+  .put("/todo/:id", updateTodoItem)
+  .delete("/todo/:id", deleteTodoItem);
 
 export default router;
