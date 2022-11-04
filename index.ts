8565a8ac148f225cb95008023d5fb43257fd4f9f
Daniil dealenx Gorodilov <dealenx@gmail.com>
2022-11-05 00:41:32 +0700

    fix: aok chanched

--- index.ts
+++ index.ts
@@ -4,10 +4,9 @@ import { oakCors } from "https://deno.land/x/cors/mod.ts";
 
 const PORT = 4000;
 const app = new Application();
-
+app.use(oakCors({ origin: "*" }));
 app.use(router.routes());
 app.use(router.allowedMethods());
-app.use(oakCors({ origin: "*" }));
 
 console.log(`App started and listening on ${PORT} `);
 
